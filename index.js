eedde28e21d6ce4c7ec66124a72db3c42aedc23e
Marcelino Lopes <marcelinodesigner34@gmail.com>
2026-09-17 20:55:37 -0300

    Converte rotas de technologies para persistencia no banco

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 });
 
-const tec = [{ "id": "js-2026-0001", "nome": "javaScript" },
-{ "id": "rc-2026-0001", "nome": "React" }, { "id": "nod-2026-0001", "nome": "Node.js" },
-]
+
 
 const profiles =[{"id": "Ar-2026-0002", "nome": "Aron Pow", "email": "aron@gmail.com", "bio": "que demais", "github": "https://github.com/aron" }, 
     {"id": "Sam-2026-0003", "nome": "Samuel Jan", "email": "samu@gmail.com", "bio": "el magnifico", "github": "https://github.com/samu"}
@@ -20,14 +18,17 @@ const projects =[{"id": "sis-2026-0004", "descricao": "sistema de automacao de n
 ]
 
 app.get("/api/technologies", async (req, res) => {
-    const resultado = await pool.query("SELECT FROM technologies");
+    const resultado = await pool.query("SELECT * FROM technologies");
     res.json(resultado.rows);
 });
 
-app.post("/api/technologies", (req, res) => {
-    const novaTec = req.body;
-    tec.push(novaTec)
-    res.status(201).json(novaTec)
+app.post("/api/technologies", async (req, res) => {
+    const resultado = await pool.query(
+        "INSERT INTO technologies (nome) VALUES ($1) RETURNING *",
+        [req.body.nome]
+
+    );
+    res.status(201).json(resultado.rows[0]);
 });
 
 app.get("/api/profiles", (req, res)=>{
