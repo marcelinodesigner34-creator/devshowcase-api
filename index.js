a2abf33abe3539a5a321e93d67f76acfee27b93a
Marcelino Lopes <marcelinodesigner34@gmail.com>
2026-09-21 10:14:38 -0300

    Converte rotas de profiles para o banco de dados

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 });
 
-
-
-const profiles =[{"id": "Ar-2026-0002", "nome": "Aron Pow", "email": "aron@gmail.com", "bio": "que demais", "github": "https://github.com/aron" }, 
-    {"id": "Sam-2026-0003", "nome": "Samuel Jan", "email": "samu@gmail.com", "bio": "el magnifico", "github": "https://github.com/samu"}
-]
-
 const projects =[{"id": "sis-2026-0004", "descricao": "sistema de automacao de notas", "nome": "NPRO", "github": "https://github.com/sis", "linkDemo": "https://lovable.dev/projects/npro", "idProfile": "Ar-2026-0002", "curtidas": 10, "notaMedia": 5.0 }, 
     {"id": "age-2026-0004", "descricao": "sistema de agendamento de projetor", "nome": "AGEPRO", "github": "https://github.com/agepro", "linkDemo": "https://lovable.dev/projects/agepro", "idProfile": "Sam-2026-0003", "curtidas": 9, "notaMedia": 5.0 }
 ]
@@ -26,30 +20,33 @@ app.post("/api/technologies", async (req, res) => {
     const resultado = await pool.query(
         "INSERT INTO technologies (nome) VALUES ($1) RETURNING *",
         [req.body.nome]
-
     );
     res.status(201).json(resultado.rows[0]);
 });
 
-app.get("/api/profiles", (req, res)=>{
-    res.json(profiles)
+app.get("/api/profiles", async (req, res)=>{
+    const resultado= await pool.query("SELECT * FROM profiles");
+    res.json(resultado.rows);
 });
 
-app.get("/api/profiles/:id", (req, res)=>{
+app.get("/api/profiles/:id", async (req, res)=>{
     const id= req.params.id;
-    const perfil =profiles.find(p => p.id === id)
-    if (!perfil){
+    const resultado =await pool.query("SELECT * FROM profiles WHERE id= $1", [id])
+    if (resultado.rows.length ===0){
         res.status(404).json({mensagem:"Perfil nao encontrado"})
     } else{
-        res.json(perfil)
+        res.json(resultado.rows[0])
     }
 });
 
-app.post("/api/profiles", (req, res)=>{
-     const novoPerfil= req.body;
-     profiles.push(novoPerfil)
-     res.status(201).json(novoPerfil)
+app.post("/api/profiles", async (req, res)=>{
+      const resultado = await pool.query(
+        "INSERT INTO profiles (nome, email, bio, github) VALUES ($1, $2, $3, $4) RETURNING *",
+        [req.body.nome, req.body.email, req.body.bio, req.body.github]
+      );
+      res.status(201).json(resultado.rows[0]);
 });
+ 
 
 app.get("/api/projects", (req, res)=>{
     res.json(projects)
